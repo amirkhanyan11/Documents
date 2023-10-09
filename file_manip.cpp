0ac1c67e7ceeb16c1d69c067ead2e9c352586fcc
amirkhanyan11 <138108895+amirkhanyan11@users.noreply.github.com>
2023-10-09 17:40:47 +0400

    Add files via upload

---
+++ file_manip.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+std::string add_something(std::string);
+std::string replace_character(std::string, char);
+
+
+int main() {
+    std::ifstream my_file;
+    my_file.open("my_file.txt");
+    std::string str;
+
+    if(!my_file.is_open()) {
+        return -1;
+    }
+
+    while(!my_file.eof()) {
+        std::getline(my_file, str);
+        std::cout << replace_character(str, 'h') << " ";
+    }
+
+    std::cout << std::endl;
+    my_file.close();
+
+    return 0;
+}
+
+
+std::string add_something(std::string str) {
+    return (str + " qwerty");
+}
+
+std::string replace_character(std::string str, char key) {
+    for(int i = 0; i < str.length(); ++i) {
+        if(str[i] == key) {
+            str[i] = '$';
+        }
+    }
+    return str;
+} 
+
