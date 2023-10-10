9ea99905ec117f03ec1ebc57938aedd23dfd7170
amirkhanyan11 <138108895+amirkhanyan11@users.noreply.github.com>
2023-10-10 16:10:38 +0400

    Update file_manip.cpp

--- file_manip.cpp
+++ file_manip.cpp
@@ -5,56 +5,44 @@ std::string add_something(std::string);
 std::string replace_character(std::string, char);
 int word_count(std::string);
 int character_count(std::string);
-void stats(std::string);
+void stats(std::ifstream&);
 
 int main() {
 
-    // std::ofstream change;
 
-    // change.open("changed.txt");
+    std::ifstream file;
 
-    // std::ifstream my_file;
-    // my_file.open("my_file.txt");
-    // std::string str;
+    file.open("text.txt");
 
-    // if(!my_file.is_open()) {
-    //     return -1;
-    // }
+    stats(file);
+    
+    file.close();
 
-    // while(!my_file.eof()) {
-    //     std::getline(my_file, str);
-    //     change << replace_character(str, 'a') << std::endl;
-    // }
+   
+    return 0;
+}
 
-    // std::cout << std::endl;
-
-    // my_file.close();
-    // change.close();
-
-    std::ifstream my_file;
+void stats(std::ifstream& file) {
 
     std::string current;
 
-    std::string res;
+    int linesCount = 0, wordCount = 0, charCount = 0;
 
-
-
-    my_file.open("my_file.txt");
-
-    while(!my_file.eof()) {
-        std::getline(my_file, current);
-        res += current;
+    while(!file.eof()) {
+        std::getline(file, current);
+        wordCount += word_count(current);
+        linesCount++;
+        charCount += character_count(current);
     }
 
-    std::cout << res << std::endl;
+    std::cout << "Lines : " << linesCount << std::endl; 
+    std::cout << "Words : " << wordCount << std::endl;
+    std::cout << "Symbols : " << charCount << std::endl;
 
-    stats(res);
 
+    return;
 
-    my_file.close();
-
-    return 0;
-}
+} 
 
 
 std::string add_something(std::string str) {
@@ -78,13 +66,14 @@ int word_count(std::string str) {
         if(str[i] != ' ') {
             word_flag = true;
         }
-        if(str[i] == ' ') {
+        if((i > 0) && (str[i] == ' ') && (str[i - 1] != ' ')) {
             count++;
             word_flag = false;
         }
     }
     return (word_flag) ? ++count : count;
 }
+
 
 int character_count(std::string str) {
     int count = 0;
@@ -96,12 +85,5 @@ int character_count(std::string str) {
     return count;
 }
 
-void stats(std::string file) {
 
-    // std::cout << "Lines : " << linesCount << std::endl; 
-    std::cout << "Words : " << word_count(file) << std::endl;
-    std::cout << "Symbols : " << character_count(file) << std::endl;
 
-    return;
-
-} 
